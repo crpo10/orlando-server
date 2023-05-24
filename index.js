57f1c19babec374fd075778ae773ccad61c62ba0
Rafael  Gomez <rgomez@contractors.ondiversity.eu>
2023-05-24 16:14:10 -0400

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const path = require('path');
 require('dotenv').config();
 const cors = require('cors');
 
-// DB config
-require('./database/config').dbConnection();
 
 // App de Express
 const app = express();
@@ -16,18 +14,11 @@ const server = require('http').createServer(app);
 app.use( cors() );
 
 // lectura y parseo del body
-app.use( express.json() );
+// app.use( express.json() );
 
 // Path público
 const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath ) );
-
-
-// Mis rutas
-app.use('/api/login', require('./routes/auth'));
-app.use('/api/car',       require('./routes/car'));
-app.use('/api/brand',       require('./routes/brand'));
-
 
 server.listen( process.env.PORT, ( err ) => {
 
